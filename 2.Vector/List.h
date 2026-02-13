b7bbc9f1577de840b01b43d2af9c33f4db2c4ae0
lelana0824 <lelana0824@gmail.com>
2026-02-13 10:10:07 +0900

    List /ListIterator 진행
    - List 기존 코드에서 문제 있는 부분 수정
    - ListIterator 만들고, 개념 설명
    - 포인터를 배열로 만들고 호가인할 때 유용한 debug 구문 수정

--- 2.Vector/List.h
+++ 2.Vector/List.h
@@ -5,10 +5,96 @@
 #include <iostream>
 #include <cassert>
 
+template<typename List>
+class ListIterator
+{
+public:
+	// List가 템플릿 파라미터로 넘어오는 경우 typename 까지 지정
+	using ValueType = typename List::ValueType;
+	using PointerType = ValueType*;
+	using ReferenceType = ValueType&;
+
+public:
+	ListIterator(PointerType ptr)
+		:ptr(ptr)
+	{
+
+	}
+
+	// 전위 증가 연산자.
+	ListIterator& operator++()
+	{
+		++ptr;
+		return *this;
+	}
+
+	// 후위 증가 연산자.
+	ListIterator& operator++(int)
+	{
+		// 현재 반복자를 임시 저장.
+		ListIterator iterator = *this;
+
+		++(*this);
+		return iterator;
+	}
+
+	ListIterator& operator--()
+	{
+		--ptr;
+		return *this;
+	}
+
+	// 후위 감소 연산자.
+	ListIterator& operator--(int)
+	{
+		// 현재 반복자를 임시 저장.
+		ListIterator iterator = *this;
+
+		--(*this);
+		return iterator;
+	}
+
+	ReferenceType operator[](int index)
+	{
+		return *(ptr + index);
+	}
+
+	PointerType operator->()
+	{
+		return ptr;
+	}
+
+	ReferenceType operator*()
+	{
+		return *ptr;
+	}
+
+	// 비교연산자 오버로딩
+	bool operator==(const ListIterator& other) const
+	{
+		return ptr == other.ptr;
+	}
+
+	bool operator!=(const ListIterator& other) const
+	{
+		return !(*this == other);
+	}
+
+private:
+	// 반복자는 결국 포인터.
+	PointerType ptr = nullptr;
+
+
+};
+
+
+
 template<typename T>
 class List
 {
-
+public:
+	using ValueType = T;
+	using Iterator = ListIterator<List<T>>;
 
 public:
 	List()
@@ -54,6 +140,25 @@ public:
 	int Size() const { return size; }
 	int Capacity() const { return capacity; }
 
+	// 범위 기반 루프 처리를 위한 함수 작성 (begin/end).
+	// cpp 컴파일러에 맞춰서 소문자로 작성
+	// 배열의 첫 위치를 반환하는 함수
+	
+	//Iterator begin()
+	T* begin()
+	{
+		return data;
+		//return Iterator(data);
+	}
+
+	// 배열에 저장된 마지막 요소의 다음 위치를 반환
+	// 마지막 요소가 아님에 주의
+	//Iterator end()
+	T* end()
+	{
+		return data + size;
+		//return Iterator(data + size);
+	}
 
 private:
 	// 저장 공간 할당하는 함수.
--- 2.Vector/Main.cpp
+++ 2.Vector/Main.cpp
@@ -6,10 +6,28 @@ int main()
 {
 
 	List<int> list;
+
 	for (int i = 0; i < 10; i++)
 	{
 		list.Add(i + 1);
 	}
+
+	std::cout << "Ranged based loop\n";
+	for (const int item : list)
+	{
+		std::cout << item << "\n";
+	}
+
+	// loop -> Iterator.
+	/*std::cout << "Iterator based loop\n";
+	for (
+		List<int>::Iterator it = list.begin();
+		it != list.end(); 
+		it++
+		)
+	{
+		std::cout << *it << "\n";
+	}*/
 
 	std::cin.get();
 }
