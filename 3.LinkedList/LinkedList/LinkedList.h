9126495f5a181145372c0b002760d269357d6171
lelana0824 <lelana0824@gmail.com>
2026-02-13 11:54:39 +0900

    LinkedList Insert/Delete/Print 함수 구현 (현재 메모리 릭 있음)
    -Insert에서 노드 삽입
    -Delete에서 노드 제거
    - Print에서 노드 순회하면서 값 출력
    - 소멸자구현해야함 (메모리정리)

--- 3.LinkedList/LinkedList/LinkedList.h
+++ 3.LinkedList/LinkedList/LinkedList.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Node.h"
+
+#include <iostream>
+
 template<typename T>
 class LinkedList
 {
@@ -11,11 +14,120 @@ public:
 		: head(nullptr), count(0)
 	{
 	}
+	~LinkedList()
+	{
+		// todo. 메모리 정리
+		NodeType current = head;
 
+		while (current)
+		{
+
+		}
+	}
+
+	void Insert(const T& data)
+	{
+		NodeType newNode = new Node<T>(data);
+
+		// 노드 삽입할 위치 확인
+
+		// 헤드가 빈 상태인지 확인.
+		if (!head)
+		{
+			head = newNode;
+		}
+		else
+		{
+			// 자리 검색을 위한 변수.
+			NodeType current = head;
+			NodeType trail = nullptr;
+
+			while (current)
+			{
+				if (current->data >= data)
+				{
+					break;
+				}
+
+
+				trail = current;
+				current = current->next;
+			}
+
+			// 검색한 위치가 헤드인 경우
+			if (current == head)
+			{
+				// newNode를 헤드로 전환
+				newNode->next = head;
+				head = newNode;
+			}
+			else
+			{
+				trail->next = newNode;
+				newNode->next = current;
+			}
+		}
+
+		count++;
+	}
+
+	void Delete(const T& data)
+	{
+		// 예외처리
+		if (!head)
+		{
+			std::cout << "List is empty";
+			return;
+		}
+
+		NodeType current = head;
+		NodeType trail = nullptr;
+
+		while (current)
+		{
+			if (current->data == data)
+			{
+				break;
+			}
+
+			trail = current;
+			current = current->next;
+		}
+
+		if (!current)
+		{
+			std::cout << "Failed to find the node\n";
+			return;
+		}
+
+		// 삭제할 노드가 헤드인 경우
+		if (head == current)
+		{
+			head = head->next;
+		}
+		else
+		{
+			trail->next = current->next;
+		}
+
+		delete current;
+		--count;
+	}
+
+
+	void Print()
+	{
+		NodeType current = head;
+
+		while (current)
+		{
+			std::cout << current->data << "\n";
+			current = current->next;
+		}
+	}
 private:
 	// 헤드
 	NodeType head = nullptr;
-
 	int count = 0;
 
 
--- 3.LinkedList/Main.cpp
+++ 3.LinkedList/Main.cpp
@@ -1,7 +1,22 @@
-
+#include <iostream>
+#include "LinkedList/LinkedList.h"
 
 int main()
 {
+	LinkedList<int> list;
 
+	list.Insert(1);
+	list.Insert(3);
+	list.Insert(2);
+	list.Insert(0);
 
+	list.Print();
+
+	list.Delete(2);
+	list.Delete(0);
+
+	std::cout << "삭제후출력\n";
+	list.Print();
+
+	std::cin.get();
 }
