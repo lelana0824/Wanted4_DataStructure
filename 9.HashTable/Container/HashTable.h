790a16b4f8cf906b14f46f4a1e679d9cfaa6e84a
lelana0824 <lelana0824@gmail.com>
2026-02-19 16:49:36 +0900

    해시테이블 구현 시작

---
+++ 9.HashTable/Container/HashTable.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Pair.h"
+#include <vector>
+#include <string>
+class HashTable
+{
+	using Entry = Pair<std::string, std::string>;
+
+public:
+	HashTable();
+	~HashTable();
+
+private:
+	// 내부 저장소 크기.
+	// - 내부 저장소 크기는 소수(1과 자기자신으로만 나눠지는 수)를 선택하는게 해시 충돌 방지에 좋음.
+	// 사용 가능한 소수 중에서 큰 수를 사용.
+	// 왜? 
+	// 나머지 연산하고 나서 결과가 겹치지 않는 수를 조사해봤더니 소수..
+	// 상용에서는 31을 많이 씀.
+	static const int bucketCount = 19;
+
+	// 이차저장소(해시 충돌 해결 방ㅂ접으로 체이닝.)
+	std::vector<Entry> table[bucketCount];
+
+
+};
