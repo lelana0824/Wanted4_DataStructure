751dde7eb59a4460c4c603a20cb182754e9c4b5e
lelana0824 <lelana0824@gmail.com>
2026-02-19 14:05:02 +0900

    Stack을 활용한 DFS 미로 찾기 진행
    - 직접 구현한 Stack 자료구조 활용
    - Sleep을 활용해 애니메이션 효과 처리
    - DFS(깊이 우선 탐색) 알고리즘 구현

---
+++ 6.StackMaze/Location2D.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Location2D
+{
+public:
+	Location2D(int row = 0, int col = 0)
+		: row(row), col(col)
+	{
+
+	}
+public:
+	int row = 0;
+	int col = 0;
+
+
+private:
+
+};
---
+++ 6.StackMaze/Main.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include "Container/Stack.h"
+#include "Location2D.h"
+#include <Windows.h>
+
+// 작은 맵.
+//const int mazeSize = 6;
+//char map[mazeSize][mazeSize] =
+//{
+//	{'1','1','1','1','1','1'},
+//	{'e','0','1','0','0','1'},
+//	{'1','0','0','0','1','1'},
+//	{'1','0','1','0','1','1'},
+//	{'1','0','1','0','0','x'},
+//	{'1','1','1','1','1','1'}
+//};
+
+// 큰 맵.
+const int mazeSize = 20;
+char map[mazeSize][mazeSize] =
+{
+	{'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1'},
+	{'1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '1', '0', '1', '0', '1'},
+	{'1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1'},
+	{'1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1'},
+	{'1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1'},
+	{'1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '1'},
+	{'e', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1'},
+	{'1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '1', '1', '1', '0', '1'},
+	{'1', '0', '1', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1'},
+	{'1', '0', '1', '0', '1', '1', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1'},
+	{'1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1'},
+	{'1', '1', '1', '1', '1', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1', '0', '1'},
+	{'1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1'},
+	{'1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1', '0', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', 'x'},
+	{'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}
+};
+
+// 방문하려는 위치가 유효한지 확인
+bool IsValidLocation(const Location2D& location)
+{
+	const int row = location.row;
+	const int col = location.col;
+
+	if (row < 0 || row >= mazeSize
+		|| col < 0 || col >= mazeSize)
+	{
+		return false;
+	}
+
+	return map[row][col] == '0'
+		|| map[row][col] == 'x';
+}
+
+void ClearScreen()
+{
+	system("cls");
+}
+
+void SetConsoleColor(WORD color)
+{
+	SetConsoleTextAttribute(
+		GetStdHandle(STD_OUTPUT_HANDLE),
+		color
+	);
+}
+
+void PrintMap(const Location2D& playerPosition,
+	DWORD delay)
+{
+	Sleep(delay);
+
+	ClearScreen();
+
+	for (int row = 0; row < mazeSize; row++)
+	{
+		for (int col = 0; col < mazeSize; col++)
+		{
+			if (row == playerPosition.row &&
+				col == playerPosition.col)
+			{
+				SetConsoleColor(FOREGROUND_GREEN);
+				std::cout << "P ";
+				SetConsoleColor(
+					FOREGROUND_BLUE |
+					FOREGROUND_GREEN |
+					FOREGROUND_RED);
+				continue;
+			}
+
+			if (map[row][col] == 'x')
+			{
+				SetConsoleColor(FOREGROUND_RED);
+				std::cout << "X ";
+				SetConsoleColor(
+					FOREGROUND_BLUE |
+					FOREGROUND_GREEN |
+					FOREGROUND_RED);
+				continue;
+			}
+
+			std::cout << map[row][col] << " ";
+		}
+
+		std::cout << "\n";
+	}
+}
+int main()
+{
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 1;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(
+		GetStdHandle(STD_OUTPUT_HANDLE),
+		&info
+	);
+
+	// 시작 위치 검색.
+	Location2D start;
+
+	bool found = false;
+	for (int row = 0; row < mazeSize; row++)
+	{
+		if (found)  break;
+		for (int col = 0; col < mazeSize; col++)
+		{
+			if (map[row][col] == 'e')
+			{
+				start.row = row;
+				start.col = col;
+				found = true;
+				break;
+			}
+		}
+	}
+
+	// 초기 맵 출력.
+	PrintMap(start, 0);
+
+	// 스택 생성
+	Stack<Location2D, mazeSize> stack;
+
+	// 시작 위치 스택에 추가.
+	stack.Push(start);
+
+	// 길찾기 (DFS).
+	// 방문 및 길찾기 진행
+	while (!stack.IsEmpty())
+	{
+		// 방문할 위치 꺼내기
+		Location2D current;
+		stack.Pop(current);
+
+		PrintMap(current, 500);
+
+		// 출구에 도착했는지 확인
+		if (map[current.row][current.col] == 'x')
+		{
+			std::cout << "\n미로 탐색 성공!\n";
+			return 0;
+			break;
+		}
+
+		// 방문 및 방문한 위치 표시
+		map[current.row][current.col] = '.';
+
+		// 방문할 지점 스택에 추가.
+		// 스택에 넣을 순서는 정할 수 있음.
+		// 상/하/좌/우 순서로 스택에 삽입.
+		if (IsValidLocation(
+			Location2D(current.row - 1, current.col)))
+		{
+			stack.Push(Location2D(current.row - 1, current.col));
+		}
+		if (IsValidLocation(
+			Location2D(current.row + 1, current.col)))
+		{
+			stack.Push(Location2D(current.row + 1, current.col));
+		}
+		if (IsValidLocation(
+			Location2D(current.row, current.col - 1)))
+		{
+			stack.Push(Location2D(current.row, current.col - 1));
+		}
+		if (IsValidLocation(
+			Location2D(current.row, current.col + 1)))
+		{
+			stack.Push(Location2D(current.row, current.col + 1));
+		}
+	}
+
+	// 길찾기 실패.
+	std::cout << "미로 탐색 실패\n";
+
+	std::cin.get();
+}
